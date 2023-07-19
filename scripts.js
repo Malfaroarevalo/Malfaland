23e82b15e2928512059c8454a06fc1fd01edf87b
Malfaroarevalo <Malfaroarevalo@gmail.com>
2023-07-19 07:02:27 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const configuraciones = document.querySelectorAll('.configuracion');
-
-    configuraciones.forEach(configuracion => {
-        configuracion.addEventListener('click', function () {
-            alert('Haz hecho clic en una configuración de computador.');
-        });
-    });
-});
