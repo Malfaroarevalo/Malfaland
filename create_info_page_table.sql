b4b9d7cc0a95b0d694b057ef6dc88abdc3035ea1
Malfaroarevalo <Malfaroarevalo@gmail.com>
2023-08-29 12:12:52 -0400

    Index Simple
    se borran las paginas de algo q había y se sube lo otro, la humanidad nunca lo sabrá?

--- create_info_page_table.sql
+++
@@ -1,7 +0,0 @@
--- Crear la tabla de información de página
-CREATE TABLE info_pagina (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  titulo VARCHAR(100) NOT NULL,
-  contenido TEXT NOT NULL,
-  tipo VARCHAR(50) NOT NULL
-);
--- create_news_table.sql
+++
@@ -1,8 +0,0 @@
--- Crear la tabla de noticias
-CREATE TABLE noticias (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  titulo VARCHAR(100) NOT NULL,
-  contenido TEXT NOT NULL,
-  autor VARCHAR(50) NOT NULL,
-  fecha_publicacion DATE NOT NULL
-);
